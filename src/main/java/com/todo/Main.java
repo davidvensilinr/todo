4396cd43303d16b8b9db3b43da6bfb435c58385b
davidvensilinr <relaxbro2005@gmail.com>
2025-09-16 13:58:20 +0530

    database connection establishment

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,16 @@
 package com.todo;
 
-public class Main{
-    public static void main(String args[]){
-        
+import com.todo.util.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        try (Connection connection = dbConnection.getDBConnection()) {
+            System.out.println("Successfully connected to the database!");
+        } catch (SQLException e) {
+            System.err.println("Database connection failed: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/todo/model/Todo.java
+++ src/main/java/com/todo/model/Todo.java
@@ -1,6 +1,5 @@
-package main.java.com.todo.model;
-
-import java.util.LocalDateTime;
+package com.todo.model;
+import java.time.LocalDateTime;
 
 public class Todo{
     private int id;
@@ -11,8 +10,8 @@ public class Todo{
     private LocalDateTime updated_at;
 
     public Todo(){
-        this.created_at=LocalDateTime.now();
-        this.updated_at=LocalDateTime.now();
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
     }
 
     public Todo(String title,String description){
@@ -22,13 +21,12 @@ public class Todo{
 
     }
 
-    public Todo(int id,String title,String description,boolean completed, LocalDateTime created_at,LocalDateTime updated_at){
+    public Todo(int id, String title, String description, boolean completed, LocalDateTime created_at, LocalDateTime updated_at){
         this.id=id;
         this.title=title;
         this.description=description;
         this.completed=completed;
         this.created_at=created_at;
-        this.updated_at=updated_at
+        this.updated_at=updated_at;
     }
-
 }
